9d6b9b842a94cfa42237f39ca1fe3a8728864b45
joaoferreira7991 <joaoferreira7991@gmail.com>
2020-04-22 21:14:04 +0100

    cleaned up environment

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/pi/repos/clone/flask-app-jf/flask-app/virtual"
+set -gx VIRTUAL_ENV "/home/pi/repos/smart_home_assistant/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
