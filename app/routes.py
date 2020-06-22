e9b3889217509ff4de7c33dc7135c0316a74746e
tufao <joaoferreira7991@gmail.com>
2020-06-22 19:45:48 +0100

    routes

--- app/routes.py
+++ app/routes.py
@@ -44,7 +44,7 @@ def sign_up():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = UserSignUpForm()
-    if form.is_submitted():
+    if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
