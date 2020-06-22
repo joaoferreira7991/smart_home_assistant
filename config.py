3b9cafd12a9468049bcdea23e47ae7d94e349de4
tufao <joaoferreira7991@gmail.com>
2020-06-22 18:04:06 +0100

    config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config(object):
     
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'kqwwieddkf2341sa'
 
-    # Creates a database 'students.db' on 
+    # Creates a database 'smart_home.db' on 
     # basedir path which is on the root of the application.
     # If it is created, then it's loaded
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'smart_home.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #or \
+        #'sqlite:///' + os.path.join(basedir, 'smart_home.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
