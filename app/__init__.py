b08a7158fb4f448cac541cb88dadb033145bb9c4
tufao <joaoferreira7991@gmail.com>
2020-07-10 15:05:41 +0100

    SocketIO test

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_socketio import SocketIO
 
 # Flask App
 app = Flask(__name__)
 app.config.from_object(Config)
+socketio = SocketIO(app)
 
 # Database Related
 # 
@@ -20,3 +22,6 @@ login = LoginManager(app)
 login.login_view = 'sign_in'
 
 from app import routes, models, forms, errors
+
+if __name__ == "__main__":
+    socketio.run(app)
