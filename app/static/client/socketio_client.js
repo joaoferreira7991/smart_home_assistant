6d7664c32635d06e0ba16aef4a56c2016667f4d0
tufao <joaoferreira7991@gmail.com>
2020-07-27 21:13:14 +0100

    Led_control

---
+++ app/static/client/socketio_client.js
@@ -0,0 +1,33 @@
+var led = document.getElementById('led');
+var led_start = document.getElementById('led_start');
+var led_colorshift = document.getElementById('led_colorshift');
+var led_increaseBrightness = document.getElementById('led_increaseBrightness');
+var led_decreaseBrightness = document.getElementById('led_decreaseBrightness');
+var led_stop = document.getElementById('led_stop');
+var socketio = io.connect(location.origin + '/client-user');
+
+led_start.innerHTML = 'On'
+led_start.onclick = function()  {
+    socketio.emit('LED_ON');
+}
+
+led_stop.innerHTML = 'Off'
+led_stop.onclick = function()  {
+    socketio.emit('LED_OFF');
+}
+
+led_colorshift.innerHTML = 'Colorshift'
+led_colorshift.onclick = function()  {
+    socketio.emit('START_COLORSHIFT');
+}
+
+led_increaseBrightness.innerHTML = 'Increase Brightness'
+led_increaseBrightness.onclick = function()  {
+    socketio.emit('INCREASE_BRIGHTNESS');
+}
+
+led_decreaseBrightness.innerHTML = 'Decrease Brightness'
+led_decreaseBrightness.onclick = function()  {
+    socketio.emit('DECREASE_BRIGHTNESS');
+}
+
