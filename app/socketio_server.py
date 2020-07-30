00d87a24b6114a6db5b57b8dd4b0ea178d365843
tufao <joaoferreira7991@gmail.com>
2020-07-30 18:20:52 +0100

    tet

--- app/socketio_server.py
+++ app/socketio_server.py
@@ -31,7 +31,7 @@ def receive_data(json_data):
 
 # Led Strip Controller
 @socketio.on('LED_ON', namespace='/client-user')
-def ledInit():
+def ledInit(handler):
     emit('LED_ON', namespace='/client-pi')
 
 @socketio.on('LED_OFF', namespace='/client-user')
