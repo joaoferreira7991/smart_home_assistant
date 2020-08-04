2a87659bec16eb20c6a916046ecf584fdd440093
tufao <joaoferreira7991@gmail.com>
2020-08-04 17:45:52 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
+# Monkey Patching
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_socketio import SocketIO, send, emit
-import eventlet, sys
-
-# Monkey Patching
-eventlet.monkey_patch()
+import sys
 
 # Flask App
 app = Flask(__name__)
