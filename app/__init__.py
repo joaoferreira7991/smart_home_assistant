4baa03876b47cbdf336d066b263117ac24079305
tufao <joaoferreira7991@gmail.com>
2020-08-04 16:14:49 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -23,9 +23,9 @@ login.login_view = 'sign_in'
 
 from app import routes, models, forms, errors, socketio_server
 
-def hello():
-    print('hello', sys.stdout)
+def hello(world:str):
+    print('hello ',world, sys.stdout)
 
 if __name__ == "__main__":
     socketio.run(app)
-    socketio.start_background_task(target=)
+    socketio.start_background_task(target=hello, args('world'))
