11ff50688cd80d3c40dfca974c2d40748f9e853f
tufao <joaoferreira7991@gmail.com>
2020-08-06 12:23:58 +0100

    test

--- utils/fix_data.py
+++ utils/fix_data.py
@@ -4,6 +4,6 @@ def fix_data(arr : list()):
     aux = list()
     for i in arr:
         print(i)
-        aux.append(i.data_reading, i.timestamp)
+        aux.append((i.data_reading, i.timestamp))
     return aux
     
