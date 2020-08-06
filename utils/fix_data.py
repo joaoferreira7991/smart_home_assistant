82d38f60086658bd98d5eaa4a86e70c30774bd8f
tufao <joaoferreira7991@gmail.com>
2020-08-06 12:13:10 +0100

    test

--- utils/fix_data.py
+++ utils/fix_data.py
@@ -3,6 +3,7 @@ from app.models import Reading
 def fix_data(arr : list()):
     aux = list()
     for i in arr:
+        print(i)
         aux.append(i.data_reading, i.timestamp)
     return aux
     
