5ba6d6b8d484c6911c650fe5d1a6ad2bc6814f78
tufao <joaoferreira7991@gmail.com>
2020-08-20 21:10:23 +0100

    test

--- app/socketio_server.py
+++ app/socketio_server.py
@@ -50,7 +50,9 @@ def loadActuator():
 # Actuator handling events
 @socketio.on('switchClick', namespace='/client-user')
 def switchClick(data):
-    print(data)
+    oActuator = Actuator.query.filter_by(id=data['id']).first()
+    if oActuator is not None:
+        print(oActuator)
 
 
 # Led Strip Controller events
