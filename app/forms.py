33f3e8293ff425527e453c564233f494ac9fea0e
tufao <joaoferreira7991@gmail.com>
2020-08-24 16:37:30 +0100

    test

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField, PasswordField, BooleanField, DateTimeField, FloatField, TimeField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
-from app.models import User
+from app.models import User, ControllerLed, Actuator
 
 data_required = 'Required Field.'
+
+# Handles unique validation errors
+class Unique(object):
+    def __init__(self, model, field, message='This element already exists.'):
+        self.model = model
+        self.field = field
+
+    def __call__(self, form, field):
+        check = self.model.query.filter(self.field == field.data).first()
+        if check:
+            raise ValidationError(self.message)
 
 class UserSignInForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(data_required)])
@@ -34,16 +45,29 @@ class ScheduleForm(FlaskForm):
     submit = SubmitField('Schedule')
 
 class ActuatorCreateForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(data_required)])
-    ip = StringField('Ip Address', validators=[DataRequired(data_required)])
+    name = StringField('Name', validators=[DataRequired(data_required),
+        Unique(
+            Actuator, 
+            Actuator.name,
+            message='This name was already used!')])
+    ip = StringField('Ip Address', validators=[DataRequired(data_required),
+        Unique(
+            Actuator, 
+            Actuator.ip, 
+            message='This ip was already assigned!')])
     submitActuator = SubmitField('Add')
 
 class ControllerCreateForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(data_required)])
+    name = StringField('Name', validators=[DataRequired(data_required),
+        Unique(
+            ControllerLed, 
+            ControllerLed.name, 
+            message='This name was already assigned!')])    
     red = IntegerField('Red GPIO Pin', validators=[DataRequired(data_required)])
     green = IntegerField('Green GPIO Pin', validators=[DataRequired(data_required)])
     blue = IntegerField('Blue GPIO Pin', validators=[DataRequired(data_required)])
     submitController = SubmitField('Add')    
+
 
 '''
 class SensorCreateForm(FlaskForm):
