c5b179aa89507324aee49a137ba8be719e90b907
tufao <joaoferreira7991@gmail.com>
2020-08-30 20:21:55 +0100

    test

--- app/forms.py
+++ app/forms.py
@@ -10,6 +10,7 @@ class Unique(object):
     def __init__(self, model, field, message='This element already exists.'):
         self.model = model
         self.field = field
+        self.message= message
 
     def __call__(self, form, field):
         check = self.model.query.filter(self.field == field.data).first()
