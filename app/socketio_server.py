09113f7d57a52bc4ebdc5e0659b1d292f3c5ef88
tufao <joaoferreira7991@gmail.com>
2020-08-30 16:18:28 +0100

    test

--- app/socketio_server.py
+++ app/socketio_server.py
@@ -20,7 +20,7 @@ def connect_user():
 def loadData(background=0, date_range=datetime.today()):
     while True:
         # Fix date_range to correctly show today without time values
-        date_range.replace(hour=0, minute=0, second=0,microsecond=0)
+        date_range = date_range.replace(hour=0, minute=0, second=0,microsecond=0)
         # Query the database
         #latestTemp = Reading.query.filter_by(data_type=data_type_dict['dht11_temperature']).order_by(Reading.id.desc()).first()
         #latestHum = Reading.query.filter_by(data_type=data_type_dict['dht11_humidity']).order_by(Reading.id.desc()).first()
