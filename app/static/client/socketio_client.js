94b0715722a38fa8e5ac7f165860daf1a0666785
tufao <joaoferreira7991@gmail.com>
2020-08-30 17:27:06 +0100

    test

--- app/static/client/socketio_client.js
+++ app/static/client/socketio_client.js
@@ -296,7 +296,7 @@ function makeChart(ctx, arr, name, min, max)    {
         data: data,
         options: {
             lineTension: 1,
-            responsive: false,
+            responsive: true,
             maintainAspectRatio: false,
             scales: {
                 xAxes:[{
