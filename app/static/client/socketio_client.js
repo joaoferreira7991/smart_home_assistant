63e7781a360a70c8206e85d38b769aade3640e16
tufao <joaoferreira7991@gmail.com>
2020-08-31 18:06:39 +0100

    forms through socketio

--- app/static/client/socketio_client.js
+++ app/static/client/socketio_client.js
@@ -93,6 +93,7 @@ $('form#actuatorform').submit(function(event)    {
         'ip'    : $('#ipActuator').val()
     }
     socketio.emit('submitForm', data=data, function(data)   {
+        alert(data);
         if(data['OK'] == 0) {
             alert(data['errors']);
         }
@@ -110,6 +111,7 @@ $('form#controllerform').submit(function(event)    {
         'blue'  : $('#blueController').val()
     }
     socketio.emit('submitForm', data=data, function(data)   {
+        alert(data);
         if(data['OK'] == 0) {
             alert(data['errors']);
         }
